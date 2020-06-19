db525f23b7af7ec281b4affbba25f2b172f8b3d1
liyiyuan <it_yiyuan@163.com>
2020-06-19 18:20:20 +0800

    disapper

---
+++ Assets/Disappper/CaculateFace.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaculateFace : MonoBehaviour {
+
+	public Material material;
+	void Start(){
+		Vector3 point = transform.position;
+		Vector3 normal = transform.right;
+		Debug.Log(normal);
+		float angle = Vector3.Dot(point, normal);
+		float length = point.magnitude * Mathf.Cos(angle);
+		Debug.Log(length);
+	}
+}
